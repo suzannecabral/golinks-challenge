bffa3a6949a18ca107a35d687ce68b4b4866a211
Suzanne Cabral <25539417+suzannecabral@users.noreply.github.com>
2021-10-20 11:32:48 -0700

    Remove context, simplify component structure

--- src/contexts/RepoContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const RepoContext = createContext({});
-// orgRepos, setOrgRepos
-// commitsUrl, setCommitsUrl
-// activeRepo, setActiveRepo
-export default RepoContext;
